ccb5e156e59aea4c7c47359f82bf48c1dfc40478
Marcel Heuberger <Infonovis2@MacBook-Business.local>
2023-01-26 16:48:48 +0100

    New work for Code Snippeds SQLs

--- APEX/SQL/SQLs_Code_Snippets.sql
+++ APEX/SQL/SQLs_Code_Snippets.sql
@@ -2,21 +2,23 @@
 select  c.name           as COLLETION_NAME
        ,c.DESCRIPTION    as COLLECTION_DESC
        ,c.INSERT_DATE    as COLLECTION_INS_DATE
-       ,c.USER           as COLLECTION_OWNER
-       ,c.PRIVAT         as COLLECTION_PRIVAT
+       ,c.OWNER          as COLLECTION_OWNER
+       ,c.PRIVATE        as COLLECTION_PRIVAT
        ,ci.KEY           as SNIP_KEY
        ,s.NAME           as SNIP_NAME
        ,s.DESCRIPTION    as SNIP_DESC
        ,g.NAME           as SNIP_GROUP
        ,s.CODE           as SNIP_CODE
        ,s.INSERT_DATE    as SNIP_INS_DATE
-       ,s.USER           as SNIP_OWNER
+       ,s.OWNER          as SNIP_OWNER
        ,s.PRIVAT         as SNIP_PRIVAT
+       ,g.DESCRIPTION    as GROUP_DESC
 from   CS_COLL_ITEM ci
 join   CS_SNIPPETS s on s.id = ci.SNIP_ID
 join   CS_COLLECTIONS c on c.id = ci.COLL_ID
 join   CS_GROUP g on g.id = s.group_id
-where  c.USER = -- input parameter: User e.g. 'hem'
+where  c.OWNER = 'HEM'
+and    c.id = 2
 
 -- Collection data per user
 select  c.name           as COLLETION_NAME
@@ -61,5 +63,56 @@ select 1,
 from   cs_snip_loader_tsr l
 join   cs_snippets s on s.code = l.value
 
+-- Insert into coll_items for a small test collection (tsr=1, hem=2)
+INSERT INTO cs_coll_item
+  (coll_id, snip_id, key)
+select 3,
+        l.id,
+        l.key
+from   cs_snip_loader_tsr l
+join   cs_snippets s on s.code = l.value
+where  rownum < 5
+
 -- set a group ID for all snippets (default -> to be cleansed)
 update cs_snippets set group_id = 3
+
+-- Create an URL with fuction in APEX
+  , apex_page.get_url(
+        p_page => '19'
+      , p_items => 'P19_ID'
+      , p_values => id)   as  card_link
+
+-- Wott eifach nöt !!!! diä Liste dynamisch füllen
+select  s.NAME           as SNIP_NAME
+       ,s.DESCRIPTION    as SNIP_DESC
+       ,ci.KEY           as SNIP_KEY
+       ,s.CODE           as SNIP_CODE
+       ,g.DESCRIPTION    as GROUP_DESC
+       ,s.INSERT_DATE    as SNIP_INS_DATE
+       ,s.PRIVAT         as SNIP_PRIVAT
+from   CS_COLL_ITEM ci
+join   CS_SNIPPETS s on s.id = ci.SNIP_ID
+join   CS_COLLECTIONS c on c.id = ci.COLL_ID
+join   CS_GROUP g on g.id = s.group_id
+where  upper(c.OWNER) = upper('&APP_USER.')
+and    c.id = 3--nvl(&ACTIV_COLL_ID,0)
+order by SNIP_INS_DATE desc
+
+--  Detail view for master detail
+
+create or replace view SNIP_DETAIL as
+select ci.coll_id        as COLL_ID
+       ,ci.id            as COLL_ITEM_ID
+       ,ci.KEY           as SNIP_KEY
+       ,s.ID             as SNIP_ID 
+       ,s.NAME           as SNIP_NAME
+       ,s.DESCRIPTION    as SNIP_DESC
+       ,s.CODE           as SNIP_CODE
+       ,g.ID             as GROUP_ID
+       ,g.DESCRIPTION    as GROUP_DESC
+       ,s.INSERT_DATE    as SNIP_INS_DATE
+       ,s.PRIVATE        as SNIP_PRIVATE
+       ,s.OWNER          as SNIP_OWNER
+from   CS_COLL_ITEM ci
+join   CS_SNIPPETS s on s.id = ci.SNIP_ID
+join   CS_GROUP g on g.id = s.group_id
