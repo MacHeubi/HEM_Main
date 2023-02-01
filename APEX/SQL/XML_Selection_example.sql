b0bb6e81922551a23800143e38a391c80a2a55f5
Marcel Heuberger <Infonovis2@MacBook-Business.local>
2023-02-01 18:01:35 +0100

    Neue SQL zur Snippet App

--- APEX/SQL/XML_Selection_example.sql
+++ APEX/SQL/XML_Selection_example.sql
@@ -1,2 +1,8 @@
 SELECT XMLELEMENT("Task", XMLATTRIBUTES (p.teilprojekt_name as "Stream", p.task_title as "DESC"), XMLELEMENT("Status", p.status), XMLELEMENT("Startdate", p.startdate))
 FROM psp_items p
+
+-- select xml_toat
+select XMLELEMENT("TEMPLATE", XMLATTRIBUTES (ci.key as "name", 'false' as "advanced"), XMLELEMENT("DESCCRIPTION", s.name), XMLELEMENT("CODE", s.code))
+from   cs_coll_item ci
+join   cs_snippets s on s.id = ci.snip_id
+where  ci.coll_id = 3
